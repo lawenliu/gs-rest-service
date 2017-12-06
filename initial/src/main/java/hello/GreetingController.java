d2c1376322b7322dd5907074dd58fbe473a2d966
lawenliu <lwc541117@gmail.com>
2017-12-05 22:13:38 -0600

    Add implementation of Restful API to inital project

---
+++ initial/src/main/java/hello/GreetingController.java
@@ -0,0 +1,33 @@
+package hello;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    /**
+     * Request with "/greeting/Body"
+     * @param name parameter
+     * @return Jackson JSON
+     */
+    @RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET)
+    public Greeting greetingRestful(@PathVariable("name") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    /**
+     * Request with "/greeting?name=Body"
+     * @param name parameter
+     * @return Jackson JSON
+     */
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greetingNormal(@RequestParam(value="name", defaultValue="World", required = true) String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
---
+++ initial/src/test/java/hello/GreetingControllerTests.java
@@ -0,0 +1,22 @@
+package hello;
+
+@Runwith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetingControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/greeting")).andDo(print()).addExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+    }
+}
